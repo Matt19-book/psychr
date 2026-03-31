65f943889b895dc5db8ce53f49f8e72fef8f356d
Matt19-book <Landondaniel1919@gmail.com>
2026-03-31 13:55:05 -0600

    Update

--- src/tabs/IRT/IRTTab.tsx
+++ src/tabs/IRT/IRTTab.tsx
@@ -9,7 +9,7 @@ import { useState } from 'react'
 import { WorkspaceLayout, PanelHeader } from '../../components/layout/WorkspaceLayout'
 import { usePsychrStore } from '../../store'
 import { useRBridge } from '../../hooks/useRBridge'
-import { ScriptPanel } from '../../components/shared/ScriptPanel'
+import { RConsole } from '../../components/shared/RConsole'
 
 type IRTModel = 'rasch' | '2pl' | '3pl' | 'grm' | 'gpcm'
 
